b1ea2db6906dd60bde4050606ce114aacee7c95b
Boobala krishnan <boobalakrishnanv@gmail.com>
2023-07-07 22:07:32 +0530

    Rotating string

--- src/com/myproject/code/StringConcept/RotationString.java
+++ src/com/myproject/code/StringConcept/RotationString.java
@@ -4,23 +4,25 @@ import java.util.Scanner;
 
 public class RotationString {
     public static void main(String[] args) throws Exception {
-        Scanner scnr = new Scanner(System.in);
+        //String 1: abcde
+        //String 2 : deabc
+        Scanner scar = new Scanner(System.in);
         System.out.println("Please enter original String");
-        String input = scnr.nextLine();
+        String input = scar.nextLine();
         System.out.println("Please enter rotation of String");
-        String rotation = scnr.nextLine();
-        if (checkRotatation(input, rotation)) {
+        String rotation = scar.nextLine();
+        if (checkRotation(input, rotation)) {
             System.out.println(input + " and " + rotation + " are rotation of each other");
         } else {
             System.out.println("Sorry, they are not rotation of another");
         }
-        scnr.close();
+        scar.close();
     }
 
     /**
      * This method check is given strings are rotation of each other * @param original * @param rotation * @return true or false
      */
-    public static boolean checkRotatation(String original, String rotation) {
+    public static boolean checkRotation(String original, String rotation) {
         if (original.length() != rotation.length()) {
             return false;
         }
