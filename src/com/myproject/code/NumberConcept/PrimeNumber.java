8ec92844376466c81ace48ac5327c06438ec225a
boobala krishnan <47085548+boobalakrishnan1998@users.noreply.github.com>
2023-08-16 11:28:13 +0530

    Update PrimeNumber.java

--- src/com/myproject/code/NumberConcept/PrimeNumber.java
+++ src/com/myproject/code/NumberConcept/PrimeNumber.java
@@ -14,58 +14,13 @@ public class PrimeNumber {
             System.out.println("Not a prime number");
             return;
         }
-        //method1(number, count);
-        // method2(number);
-        //print n prime number
-        for(int i=1;number>count;i++){
-            int curNum=0;
-            int temp=i;
-            for(int j = 2; j <= temp; j++){
-                if(temp % j ==0){
-                    curNum= j;
-                    break;
-                }
-            }
-            if(temp ==curNum){
-                System.out.print(temp+" ");
-                count++;
-            }
-        }
+        lessTimeComplex(number);
     }
-
-    private static void method2(int number) {
-        int curNum=0;
-        for(int i = 2; i<= number; i++){
-
-            if(number %i==0){
-                curNum=i;
-                break;
-            }
+    public void lessTimeComplex(int num) {
+         if(number==0||number==1){
+            System.out.println("Not a prime number");
+            return;
         }
-        if(number ==curNum){
-            System.out.println("Given number is Prime number ");
-        }else{
-           System.out.println("Given number is not a prime number ");
-        }
-    }
-
-    private static void method1(int number, int count) {
-        for (int i = 1; i<= number; i++){
-           if(number %i==0){
-               count++;
-           }
-        }
-        if(count ==2){
-            System.out.println("Given number is Prime number ");
-        }else{
-            System.out.println("Given number is not a prime number ");
-        }
-    }
-
-
-    public void lessTimeComplex() {
-
-        int num = 29;
         boolean flag = false;
         for (int i = 2; i <= num / 2; ++i) {
             // condition for nonprime number
@@ -74,14 +29,9 @@ public class PrimeNumber {
                 break;
             }
         }
-
         if (!flag)
             System.out.println(num + " is a prime number.");
         else
             System.out.println(num + " is not a prime number.");
     }
-
-
-
-
 }
