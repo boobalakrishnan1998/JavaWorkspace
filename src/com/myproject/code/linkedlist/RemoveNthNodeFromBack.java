c9c3e44a5972562b9d9d5433d67e9af9304243fb
Boobala Krishnan <boobalakrishnanv@gmail.com>
2024-08-05 15:42:15 +0530

    DSA code sheet LL

--- src/com/myproject/code/linkedlist/RemoveNthNodeFromBack.java
+++ src/com/myproject/code/linkedlist/RemoveNthNodeFromBack.java
@@ -5,13 +5,55 @@ import static com.myproject.code.linkedlist.InsertInDLL.printLL;
 
 public class RemoveNthNodeFromBack {
     public static void main(String[] args) {
-        int[] arr1 = new int[]{1, 2, 3, 4, 5, 6, 7};
-        Node head1 = convertToDLL(arr1);
-        Node temp = removeNthElementFromBack(head1, 7);
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Node head = convertToDLL(arr);
+        //Node temp = removeNthElementFromBack(head1, 7);
+        Node temp = removeNthElementFromBackOptimal(head, 5);//Optimal approach
+        // Node temp = removeNthElementFromBackOptimal2(head, 5);//Optimal approach
         printLL(temp);
     }
 
-    private static Node removeNthElementFromBack(Node head, int n) {
+    private static Node removeNthElementFromBackOptimal(Node head, int n) {
+        Node slow = head;
+        Node fast = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        if (fast == null) {
+            return head.next;
+        }
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        Node delNode = slow.next;
+        slow.next = slow.next.next;
+        delNode = null;
+        return head;
+    }
+
+    private static Node removeNthElementFromBackOptimal2(Node head, int n) {
+        Node temp = head;
+        int right = 0, left = 0;
+        Node leftNode = temp;
+        while (temp != null) {
+            right++;
+            if (right > n + 1) {
+                left++;
+                leftNode = leftNode.next;
+            }
+            temp = temp.next;
+        }
+        if (leftNode == null)
+            return null;
+        if (leftNode == head) {
+            head = head.next;
+        }
+        leftNode.next = leftNode.next.next;
+        return head;
+    }
+
+    private static Node removeNthElementFromBack(Node head, int n) {//brute force method
         Node temp = head;
         int length = 0;
         while (temp != null) {
